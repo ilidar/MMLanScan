3e91c1d17614b8e77b4952a788846dc1ba735837
Denys Kotelovych <denys.kotelovych@gmail.com>
2017-10-27 00:16:56 +0300

    One project to rule them all;

---
+++ MMLanScan/MMLanScan.h
@@ -0,0 +1,30 @@
+//
+//  MMLanScan.h
+//  MMLanScan
+//
+//  Created by Denys Kotelovych on 10/27/17.
+//  Copyright © 2017 Denys Kotelovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMLanScan.
+FOUNDATION_EXPORT double MMLanScanVersionNumber;
+
+//! Project version string for MMLanScan.
+FOUNDATION_EXPORT const unsigned char MMLanScanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMLanScan/PublicHeader.h>
+
+#ifdef __OBJC__
+    #import "MMLANScanner.h"
+    #import "MACOperation.h"
+    #import "PingOperation.h"
+    #import "NetworkCalculator.h"
+    #import "LANProperties.h"
+    #import "MacFinder.h"
+    #import "SimplePing.h"
+    #import "if_arp.h"
+    #import "if_ether.h"
+    #import "route.h"
+#endif
